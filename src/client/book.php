17addac1527898cb7208c93564e6011b3df38372
arfahh <arfahhuda@gmail.com>
2024-07-28 04:08:20 -0400

    updates

--- src/client/book.php
+++ src/client/book.php
@@ -7,14 +7,16 @@ $dbname = "hotelbookingmanagement";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+
 $room_id = $_POST['room_id'] ?? '';
 $arrival = $_POST['arrival'] ?? '';
 $departure = $_POST['departure'] ?? '';
+
 
 if (empty($room_id) || empty($arrival) || empty($departure)) {
     echo json_encode(['success' => false, 'message' => 'Missing required fields']);
